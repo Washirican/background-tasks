3fbe3ebbf851fbaab54d13552063907aef392ca8
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2021-04-13 16:03:38 -0700

    Restart.

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask
-from celery import Celery
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2021-04-13, File created.
+# --------------------------------------------------------------------------- #
+from rq import Queue
+from worker import conn
+from utils import count_words_at_url
 
-app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+q = Queue(connection=conn)
 
-<<<<<<< HEAD
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
-=======
 result = q.enqueue(count_words_at_url, 'http://heroku.com')
-print(result)
->>>>>>> e5ac0772f2188b7f024c4b2a81b24fba68417aa3
+
--- worker.py
+++ worker.py
@@ -1,3 +1,6 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2021-04-13, File created.
+# --------------------------------------------------------------------------- #
 import os
 
 import redis
@@ -6,14 +9,11 @@ from rq import Worker, Queue, Connection
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-conn = redis.from_url(redis_url)
-<<<<<<< HEAD
 
-# conn = redis.Redis('localhost', 6379, 0)
-=======
->>>>>>> e5ac0772f2188b7f024c4b2a81b24fba68417aa3
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
+
